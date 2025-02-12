f865dc5926e0fa6849ca86c91faa3ba77ee21582
Tenkou1015 <102134985+Tenkou1015@users.noreply.github.com>
2025-02-13 01:18:29 +0900

    Add files via upload

--- assets/js/menu-toggle.js
+++ assets/js/menu-toggle.js
@@ -1,16 +1,14 @@
 document.addEventListener("DOMContentLoaded", function() {
     const menuToggle = document.querySelector(".menu-toggle");
     const navUl = document.querySelector("nav ul");
-
+    
     menuToggle.addEventListener("click", function() {
-        navUl.style.display = navUl.style.display === "flex" ? "none" : "flex";
+        navUl.classList.toggle("show-menu");
     });
-
+    
     window.addEventListener("resize", function() {
         if (window.innerWidth >= 769) {
-            navUl.style.display = "flex"; // 大屏幕时显示导航栏
-        } else {
-            navUl.style.display = "none"; // 小屏幕时隐藏导航栏
+            navUl.classList.remove("show-menu");
         }
     });
-}); 
+});
